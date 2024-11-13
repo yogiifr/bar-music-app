1f6520b1ccfa2834eb13d3ecb785b6a4a1054e30
yogiifr <yogiffrr@gmail.com>
2024-11-13 19:41:41 +0700

    add: backend config

---
+++ backend/src/index.js
@@ -0,0 +1,15 @@
+import express from "express";
+import dotenv from "dotenv";
+
+import userRoutes from "./routes/user.route.js";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT;
+
+app.use("/api/users", userRoutes);
+
+app.listen(PORT, () => {
+  console.log("Server is running on port " + PORT);
+});
